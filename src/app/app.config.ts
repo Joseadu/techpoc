427981b061e05d2f4c50686de0f96ed190ce6c1c
Jose Antonio Diaz Urbano <jadiaz@knowmadmood.com>
2026-04-14 09:55:04 +0200

    fix(core): migrate zoneless and translate config to modern standalone apis

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,35 +1,27 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
-  importProvidersFrom,
+  provideZonelessChangeDetection,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './i18n/', '.json');
-}
-
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withFetch()),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        defaultLanguage: 'es',
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
-      }),
-    ),
+    provideTranslateService({
+      defaultLanguage: 'es',
+    }),
+    provideTranslateHttpLoader({
+      prefix: './i18n/',
+      suffix: '.json',
+    }),
   ],
 };
